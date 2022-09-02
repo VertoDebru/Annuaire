c3e790db0cf0d04ceba942877ab6750c2fbe70d9
VertoDebru <t.vervoot@gmail.com>
2022-09-02 11:34:39 +0200

    Update type for phone.

--- back/models/Contacts.js
+++ back/models/Contacts.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema({
     name: { type: String, required: true },
-    phone: { type: Number, required: true }
+    phone: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Contacts', contactSchema);
